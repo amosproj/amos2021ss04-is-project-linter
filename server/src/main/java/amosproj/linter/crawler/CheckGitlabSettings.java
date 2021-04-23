1f9577251ac5731d424a1b435716286e7efad1c1
Christoph Wohlwend <c.wohlwend@gmx.de>
2021-04-23 22:34:08 +0200

    crawler doesn't need an id now

--- server/src/main/java/amosproj/linter/crawler/CheckGitlabSettings.java
+++ server/src/main/java/amosproj/linter/crawler/CheckGitlabSettings.java
@@ -1,21 +1,28 @@
 package amosproj.linter.crawler;
 
 import amosproj.linter.crawler.consumptions.ProjectInformationJson;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 public class CheckGitlabSettings {
 
   public static boolean isPublic(String apiUrl) {
     // call api
+    java.net.URI test;
     RestTemplate restTemplate = new RestTemplate();
     ProjectInformationJson info;
     try {
-      info = restTemplate.getForObject(apiUrl, ProjectInformationJson.class);
-    } catch (HttpClientErrorException e) {
+      test = new URI(apiUrl);
+      info = restTemplate.getForObject(test, ProjectInformationJson.class);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
       return false;
     }
+
     // if public return true
-    return info != null && info.getVisibility().equals("public");
+    return  info != null && info.getVisibility() != null && info.getVisibility().equals("public");
   }
 }
--- server/src/main/java/amosproj/linter/crawler/Crawler.java
+++ server/src/main/java/amosproj/linter/crawler/Crawler.java
@@ -1,15 +1,20 @@
 package amosproj.linter.crawler;
 
-import amosproj.linter.server.data.*;
+import amosproj.linter.server.data.LintingResult;
+import amosproj.linter.server.data.Project;
+import amosproj.linter.server.data.ProjectRepository;
 import amosproj.linter.server.services.BeanUtil;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 
 public class Crawler {
-  // this function is only for testing purpouses
+  // this function is only for testing purposes
   public static void testCrawler() {
     getResult("https://gitlab.com/altaway/herbstluftwm");
+//    getResult("https://gitlab.com/tnir/www-gitlab-com");
+//    getResult("https://gitlab.com/aiakos/python-openid-connect");
   }
 
   // entry point for api
@@ -51,70 +56,78 @@ public class Crawler {
 
     // get correct API URL
     String apiUrl;
-    if (hostedByGitlab(URL)) {
-      // hosted by gitlab.com
-      apiUrl = getApiUrlForGitlabDotComProject(URL);
-    } else {
-      // hosted by gitlab.example.com (gitlab instance)
-      apiUrl = getApiUrlForGitlabInstanceProject(URL);
+    if (!hostedByGitlab(URL)){
+      return;
     }
+    apiUrl = getApiUrlForGitlab(URL);
 
-    // Actually start doing work
+    // Actually start doing work with the api
     // Starting with Gitlab Settings Check
-    SettingsCheck settingsCheck = createSettingsCheckObject(lintingResult);
-    settingsCheck.setPublic(CheckGitlabSettings.isPublic(apiUrl));
-
-    // save checked data to db
-    saveSettingsCheckObject(settingsCheck);
+    // todo: waiting for DB Team to implement SettingsCheck Table so we can save it to a settings Object
+    // will be something like:
+//    SettingsCheck settingsCheck = createSettingsCheckObject(lintingResult);
+//    lintingGitlabSettings.setPublic(CheckGitlabSettings.isPublic(apiUrl));
+    CheckGitlabSettings.isPublic(apiUrl);
   }
 
-  private static void saveSettingsCheckObject(SettingsCheck settingsCheck) {
-    SettingsCheckRepository settingsCheckRepository = BeanUtil.getBean(SettingsCheckRepository.class);
-    settingsCheckRepository.save(settingsCheck);
-  }
-
-  private static SettingsCheck createSettingsCheckObject(LintingResult lintingResult) {
-    return new SettingsCheck(lintingResult, false, false, false, false, false);
-  }
-
-  private static String getApiUrlForGitlabInstanceProject(String url) {
+  private static String getApiUrlForGitlab(String url) {
+    String result = "";
     // Insert /api/v4 before the 3rd "/" (cause the first two are https://)
+    // and encode the / in the url of the repository to %2F
     String[] parts = url.split("/");
-    String[] newParts = new String[parts.length + 1];
-    int j = 0;
-    for (int i = 0; i < parts.length; i++) {
-      newParts[j] = parts[i];
-      if (i == 2) {
-        newParts[j + 1] = "api/v4";
-        j++;
-      }
-      j++;
+    result = "https://" + parts[2] + "/api/v4/projects/";
+    for (int i = 3; i < parts.length - 1; i++) {
+      result += parts[i] + "%2F";
     }
-
-    StringBuilder sb = new StringBuilder();
-    for (String part :
-        newParts) {
-      sb.append(part);
-      sb.append('/');
-    }
-
-    return sb.toString();
+    result += parts[parts.length - 1];
+    return result;
   }
+
+//  private static String getApiUrlForGitlabInstanceProject(String url) {
+//    // Insert /api/v4 before the 3rd "/" (cause the first two are https://)
+//    String[] parts = url.split("/");
+//    String[] newParts = new String[parts.length + 1];
+//    int j = 0;
+//    for (int i = 0; i < parts.length; i++) {
+//      newParts[j] = parts[i];
+//      if (i == 2) {
+//        newParts[j + 1] = "api/v4";
+//        j++;
+//      }
+//      j++;
+//    }
+//
+//    StringBuilder sb = new StringBuilder();
+//    for (String part :
+//        newParts) {
+//      sb.append(part);
+//      sb.append('/');
+//    }
+//
+//    return sb.toString();
+//  }
 
   private static boolean hostedByGitlab(String url) {
+    //check if url is gitlab.com
+    String[] parts = url.split("/");
+    return parts[2].matches("gitlab\\.com") || parts[2].matches("gitlab\\..*\\.com");
     // check if url is gitlab.com or gitlab.example.com
-    String[] parts = url.split("\\.");
+    //String[] parts = url.split("\\.");
     // return true if gitlab.com, false if not
-    if (parts[0].equals("https://gitlab")) return true;
-    return parts[1].equals("gitlab");
+//    if (parts[0].equals("https://gitlab")) return true;
+//    return parts[1].equals("gitlab");
   }
 
-  private static String getApiUrlForGitlabDotComProject(String URL) {
-    //todo implement this, ONLY GIVES BACK DUMMY ANSWER
-//    // example ID  URL?: https://gitlab.com/api/v4/projects/26063188
-//    long id = getGitlabDotComProjectId(URL); // i dont know how to do this right now?!
-//    return "https://gitlab.com/api/v4/projects/" + id + "/";
-    return "https://gitlab.com/api/v4/projects/26063188/";
-  }
+//  private static String getApiUrlForGitlabDotComProject(String URL) {
+//    System.out.println(URL);
+//
+//    //todo implement this, ONLY GIVES BACK DUMMY ANSWER
+//
+////    // example ID  URL?: https://gitlab.com/api/v4/projects/26063188
+////    long id = getGitlabDotComProjectId(URL); // i dont know how to do this right now?!
+////    return "https://gitlab.com/api/v4/projects/" + id + "/";
+// //   return "https://gitlab.com/api/v4/projects/26063188?access_token=dvmC-3KRRLsSXRbH63r";
+//    return "https://gitlab.com/api/v4/projects/kalilinux%2Fpackages%2Ftyper";
+// //   return "https://gitlab.com/api/v4/projects/26063188/";
+//  }
 }
-
