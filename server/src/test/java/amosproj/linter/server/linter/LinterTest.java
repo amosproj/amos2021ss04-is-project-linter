49d726504e0a1a58f710d59f388ce94f365f032c
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-24 00:27:08 +0200

    :white_check_mark: fix tests
    and write new ones

---
+++ server/src/test/java/amosproj/linter/server/linter/LinterTest.java
@@ -0,0 +1,40 @@
+package amosproj.linter.server.linter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@SpringBootTest
+public class LinterTest {
+
+    private String[] getTestRepos() {
+        Resource resource = new ClassPathResource("classpath:repoList.txt");
+        try {
+            InputStream inputStream = resource.getInputStream();
+            byte[] binaryData = FileCopyUtils.copyToByteArray(inputStream);
+            String data = new String(binaryData, StandardCharsets.UTF_8);
+            return data.split("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String[]{};
+    }
+
+    @Test
+    public void testGetResult() {
+        Linter cw = new Linter();
+
+        for (String repo : getTestRepos()) {
+            var actual = cw.getResult(repo);
+            System.out.println(actual);
+        }
+
+    }
+
+}
