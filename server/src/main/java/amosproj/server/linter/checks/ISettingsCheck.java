850352d786c6b24c4d7695d5a780ee1b75cdba65
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-26 20:16:47 +0200

    Update ISettingsCheck.java

--- server/src/main/java/amosproj/server/linter/checks/ISettingsCheck.java
+++ server/src/main/java/amosproj/server/linter/checks/ISettingsCheck.java
@@ -1,10 +1,13 @@
 package amosproj.server.linter.checks;
 
-import amosproj.server.linter.consumptions.ProjectInformationJson;
+import org.springframework.boot.ApplicationContextFactory;
 
 interface ISettingsCheck {
 
-    boolean settingFieldIsSet();
+    /**
+     * checks if a settings field has the expected value
+     */
+    boolean checkValue(org.gitlab4j.api.models.Project project, String field, String expected);
 
 
 
