4d689d7a4f10671b0489f83303dfb128628c5268
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-27 18:12:51 +0200

    tests now use a common properties file
    common configuration for all tests

--- server/src/test/java/amosproj/server/data/ProjectTest.java
+++ server/src/test/java/amosproj/server/data/ProjectTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
 @AutoConfigureDataJpa
+@TestPropertySource(locations="classpath:test.properties")
 public class ProjectTest {
 
     @Autowired
--- server/src/test/java/amosproj/server/linter/LinterTest.java
+++ server/src/test/java/amosproj/server/linter/LinterTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
@@ -12,13 +13,14 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class LinterTest {
 
     @Autowired
     private Linter linter;
 
     private String[] getTestRepos() {
-        Resource resource = new ClassPathResource("classpath:repoList.txt");
+        Resource resource = new ClassPathResource("repoList.txt");
         try {
             InputStream inputStream = resource.getInputStream();
             byte[] binaryData = FileCopyUtils.copyToByteArray(inputStream);
