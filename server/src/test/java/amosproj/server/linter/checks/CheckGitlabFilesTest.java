1b0ef5d3b3801ec8937ee86418de08e17807c38a
Christoph Wohlwend <c.wohlwend@gmx.de>
2021-05-03 21:53:59 +0200

    Added a new Test for checking the existance of a readme file

--- server/src/test/java/amosproj/server/linter/checks/CheckGitlabFilesTest.java
+++ server/src/test/java/amosproj/server/linter/checks/CheckGitlabFilesTest.java
@@ -1,12 +1,71 @@
 package amosproj.server.linter.checks;
 
+import amosproj.server.GitLab;
+import amosproj.server.data.LintingResult;
+import amosproj.server.data.Project;
+import amosproj.server.data.ProjectRepository;
+import amosproj.server.linter.Linter;
+import org.gitlab4j.api.GitLabApiException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:test.properties")
 public class CheckGitlabFilesTest {
 
+    @Autowired
+    private Linter linter;
 
+    @Autowired
+    private GitLab api;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    private CheckGitlabFiles checkGitlabFiles;
+
+    private void preparePositive() throws GitLabApiException {
+        prepareSettingsCheck("https://gitlab.cs.fau.de/or16iqyd/hasReadme");
+    }
+
+    private void prepareNegative() throws GitLabApiException {
+        prepareSettingsCheck("https://gitlab.cs.fau.de/or16iqyd/noReadme");
+    }
+
+    private void prepareSettingsCheck(String repoUrl) throws GitLabApiException {
+        repoUrl = repoUrl.replace("\r", "");
+        String path = repoUrl.replace(api.getGitlabHost() + "/", "");
+
+        org.gitlab4j.api.models.Project proj = api.getApi().getProjectApi().getProject(path);
+        assert proj != null;
+
+        Project currLintingProject = projectRepository.findByGitlabProjectId(proj.getId());
+        if (currLintingProject == null) {
+            currLintingProject = new Project(proj.getName(), proj.getWebUrl(), proj.getId(), api.getGitlabHost());
+            projectRepository.save(currLintingProject);
+        }
+
+        var lintingResult = new LintingResult(currLintingProject, LocalDateTime.now());
+        this.checkGitlabFiles = new CheckGitlabFiles(api.getApi(), lintingResult, proj, linter.getConfig());
+    }
+
+    @Test
+    void test_hasRequestAccessEnabled_positive() throws GitLabApiException {
+        preparePositive();
+        assertTrue(checkGitlabFiles.checkReadmeExistence());
+    }
+
+    @Test
+    void test_hasRequestAccessEnabled_negative() throws GitLabApiException {
+        prepareNegative();
+        assertFalse(checkGitlabFiles.checkReadmeExistence());
+    }
 
 }
