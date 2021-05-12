7d6a667525c04d96f7e2a91247bd3467956d90b3
Christoph Wohlwend <c.wohlwend@gmx.de>
2021-05-12 22:34:25 +0200

    added checks and tests for link checking in readme and contributing, regexs have to be adjusted/improved

--- server/src/main/java/amosproj/server/linter/checks/CheckGitlabFiles.java
+++ server/src/main/java/amosproj/server/linter/checks/CheckGitlabFiles.java
@@ -6,6 +6,10 @@ import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.RepositoryFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CheckGitlabFiles extends Check {
 
     private org.gitlab4j.api.models.Project proj;
@@ -39,6 +43,64 @@ public class CheckGitlabFiles extends Check {
             return false;
         }
         return true;
+    }
+
+    public boolean checkContributingHasLinks(){
+        //lade Datei in java.io.tmp
+        File file = getRawFile("CONTRIBUTING.md");
+        if(file != null) {
+            try{
+                Scanner scanner = new Scanner(file);
+                //lese Zeile der Datei bis Ende
+                while (scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    //suche in Zeile nach gewünschten Inhalt
+                    //this needs to be adjusted
+                    if(line.matches(".*CONTRIBUTING.md.*")){
+                        return true;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+
+            }
+        }
+        return false;
+    }
+
+    public boolean checkReadmeHasLinks(){
+        //lade Datei in java.io.tmp
+        //adjust this to work with proj.getReadmeUrl() instead of README.md
+        File file = getRawFile("README.md");
+        if(file != null) {
+            try{
+                Scanner scanner = new Scanner(file);
+                //lese Zeile der Datei bis Ende
+                while (scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    //suche in Zeile nach gewünschten Inhalt
+                    //this needs to be adjusted
+                    if(line.matches(".*http://.*\\..*")){
+                        return true;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+
+            }
+        }
+        return false;
+    }
+
+    public File getRawFile(String filepath) {
+        if(fileExists(filepath)) {
+            try{
+                //lade die Datei nach java.io.tmp
+                File file = api.getRepositoryFileApi().getRawFile(proj.getId(), proj.getDefaultBranch(), filepath, null);
+                return file;
+            } catch (GitLabApiException e) {
+                System.out.println("reason: " + e.getReason());
+            }
+        }
+        return null;
     }
 
 }
