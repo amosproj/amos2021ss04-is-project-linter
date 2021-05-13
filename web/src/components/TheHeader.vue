0fa0c6a10da8180c3f4ccd5dd3d311aaf97377d7
Lukas Böhm <boehm-lukas@outlook.de>
2021-05-13 22:20:34 +0200

    :lipstick:  new header

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -1,8 +1,16 @@
 <template>
-	<v-app-bar app color="primary" dark>
-		<div class="d-flex align-center">
-			ACIDIC
-		</div>
+	<v-app-bar app color="primary" flat>
+		<v-container class="py-0 fill-height">
+			<v-btn text to="/">
+				ACIDIC
+			</v-btn>
+
+			<v-spacer></v-spacer>
+
+			<v-responsive max-width="260">
+				<v-text-field dense flat hide-details rounded solo-inverted placeholder="Suche ..."></v-text-field>
+			</v-responsive>
+		</v-container>
 	</v-app-bar>
 </template>
 
