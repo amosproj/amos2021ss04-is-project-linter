4aa9f91f4ffa949c1d8a4f9be8c71d056b4dd9ae
Lukas Böhm <boehm-lukas@outlook.de>
2021-05-16 18:10:37 +0200

    :sparkles:  add Footer

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,15 +8,18 @@
 			</v-container>
 			<router-view />
 		</v-main>
+
+		<TheFooter />
 	</v-app>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import Events from "./components/Events.vue";
+import TheFooter from './components/TheFooter.vue';
 
 export default {
-	components: { TheHeader, Events },
+	components: { TheHeader, Events, TheFooter },
 	name: "App",
 };
 </script>
---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-footer color="primary">
+        <div>
+            Bei Fragen oder Feedback:<br>
+            ria.inner.source@datev.com
+        </div>
+        <v-spacer></v-spacer>
+        <div>
+            developed by: <strong>ACIDIC</strong>
+        </div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+}
+</script>
