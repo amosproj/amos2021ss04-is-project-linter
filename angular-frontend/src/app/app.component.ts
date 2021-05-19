b45132d9909c55bb2d2d90759192a22e174e837d
Lukas Böhm <boehm-lukas@outlook.de>
2021-05-19 11:56:04 +0200

    fix base url

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -3,115 +3,110 @@ import { ComponentFactoryResolver } from '@angular/core';
 import { ViewContainerRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { RepositoryComponent } from './repository/repository.component';
-import { HttpClient, HttpClientModule,HttpHeaders } from '@angular/common/http';
-import {MatDialogModule, MatDialog} from '@angular/material/dialog'; 
-
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
   title = 'angular-frontend';
   //SearchBarValue = '';
-  all_projects:Project[];
-  serverID = "http://localhost:8080/"
+  all_projects: Project[];
+  serverID = 'http://localhost:6969/api/';
   options: FormGroup;
   forwardLinkWorked = true;
-  errorMsgForwardLink = "";
+  errorMsgForwardLink = '';
 
   hideRequiredControl = new FormControl(false);
   floatLabelControl = new FormControl('auto');
   @ViewChild('parent', { read: ViewContainerRef }) container: ViewContainerRef;
   //onEnter(SearchBarValue: string) { this.SearchBarValue = SearchBarValue;
-  //this.forwardLink("http://localhost:8080/projects",SearchBarValue);
+  //this.forwardLink("http://localhost:6969/api/projects",SearchBarValue);
   //}
- 
-constructor(fb: FormBuilder,private _cfr: ComponentFactoryResolver,private http: HttpClient) {
-  this.options = fb.group({
-    hideRequired: this.hideRequiredControl,
-    floatLabel: this.floatLabelControl,
-  });
-}
 
-getIfForwardLinkWorked(){
-  return this.forwardLinkWorked;
-}
-  forwardLink(serverID,URL){
-    const headers = { 'Content-Type': 'text/html'}  
+  constructor(
+    fb: FormBuilder,
+    private _cfr: ComponentFactoryResolver,
+    private http: HttpClient
+  ) {
+    this.options = fb.group({
+      hideRequired: this.hideRequiredControl,
+      floatLabel: this.floatLabelControl,
+    });
+  }
 
-    let HTTPOptions:Object = {
+  getIfForwardLinkWorked() {
+    return this.forwardLinkWorked;
+  }
+  forwardLink(serverID, URL) {
+    const headers = { 'Content-Type': 'text/html' };
 
+    let HTTPOptions: Object = {
       headers: new HttpHeaders({
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       }),
-      responseType: 'text'
-   }
+      responseType: 'text',
+    };
 
-    this.http.post<String>(serverID,URL,HTTPOptions)
-    .subscribe(
-        (val:any) => {
-            console.log("POST call successful value returned in body", 
-                        val);
-            var regex404 = new RegExp("404 NOT_FOUND","i")
-            console.log(val.search(regex404))
-            if(val.search(regex404) != -1){
-              this.errorMsgForwardLink = 'Fehler 404, bitte URL überprüfen'
-              this.forwardLinkWorked = false;
-              console.log(this.forwardLinkWorked)
-
-            }else{
-            this.forwardLinkWorked = true;
-            }
-            console.log(this.forwardLinkWorked)
-        },
-        error => {
-            console.log("POST call in error", error);
-            this.errorMsgForwardLink = 'Internal server error'
-            this.forwardLinkWorked = false;
+    this.http.post<String>(serverID, URL, HTTPOptions).subscribe(
+      (val: any) => {
+        console.log('POST call successful value returned in body', val);
+        var regex404 = new RegExp('404 NOT_FOUND', 'i');
+        console.log(val.search(regex404));
+        if (val.search(regex404) != -1) {
+          this.errorMsgForwardLink = 'Fehler 404, bitte URL überprüfen';
+          this.forwardLinkWorked = false;
+          console.log(this.forwardLinkWorked);
+        } else {
+          this.forwardLinkWorked = true;
         }
-        /*() => {
+        console.log(this.forwardLinkWorked);
+      },
+      (error) => {
+        console.log('POST call in error', error);
+        this.errorMsgForwardLink = 'Internal server error';
+        this.forwardLinkWorked = false;
+      }
+      /*() => {
             console.log("The POST observable is now completed.");
             this.errorMsgForwardLink = 'Internal server error'
             this.forwardLinkWorked = false;
         }*/
-        );
-}
+    );
+  }
 
-
-GetProjects(serverID){
-  this.http.get(serverID).subscribe(
-
-    results => {
+  GetProjects(serverID) {
+    this.http.get(serverID).subscribe((results) => {
       this.all_projects = JSON.parse(JSON.stringify(results)) as Project[];
       console.log(this.all_projects);
 
-      for(let project in this.all_projects){
-       this.addComponent(this.all_projects[project].name,this.all_projects[project].id,this.all_projects[project].url);
+      for (let project in this.all_projects) {
+        this.addComponent(
+          this.all_projects[project].name,
+          this.all_projects[project].id,
+          this.all_projects[project].url
+        );
       }
-    }
-
-  );
-  /*{ // currently it you can only send the pure URL and not as a JSON
+    });
+    /*{ // currently it you can only send the pure URL and not as a JSON
       "data": gitID
   })*/
-  
-    }
+  }
 
+  ngOnInit() {
+    this.GetProjects('http://localhost:6969/api/projects');
+  }
 
-
-
-ngOnInit(){ 
-  this.GetProjects("http://localhost:8080/projects");
-
-}
-
-
-  addComponent(name, id, gitlabInstance){    
+  addComponent(name, id, gitlabInstance) {
     var comp = this._cfr.resolveComponentFactory(RepositoryComponent);
     var expComponent = this.container.createComponent(comp);
     expComponent.instance._ref = expComponent;
@@ -119,24 +114,16 @@ ngOnInit(){
     expComponent.instance.id = id;
     expComponent.instance.gitlabInstance = gitlabInstance;
     expComponent.instance.serverID = this.serverID;
-}
-
-
-
+  }
 } // end of AppComponent
-
 
 // Interface for the repository component which shows coarse repo infos
 interface Project {
+  gitlabInstance: string;
+  gitlabProjectId: number;
 
-  
-  gitlabInstance: string,
-   gitlabProjectId:number,
-
-   id: number,
-   name: string,
-   results:[],
-   url:string
-
+  id: number;
+  name: string;
+  results: [];
+  url: string;
 }
-
--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ax = axios.create({
-    baseURL: process.env.VUE_APP_API || "http://localhost:8080",
+    baseURL: process.env.VUE_APP_API || "http://localhost:6969/api",
     // headers: {
     //     "Content-type": "application/json",
     // },
