96e5238ee63d98d6b6c71cbb478301bf9112dbe0
Lukas Böhm <boehm-lukas@outlook.de>
2021-05-25 22:33:59 +0200

    :whale: fix docker for frontend

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:6969/api',
+  baseURL: 'http://localhost:6969/api', // TODO make the port configurable. Take the env variable from docker-compose
 };
