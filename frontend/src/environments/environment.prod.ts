159076f3001bff288a663453b48dd80b722efe29
Lukas <boehm-lukas@outlook.de>
2021-06-07 23:47:07 +0200

    :sparkles: :wrench: config file reachable via endpoint
    call `/config`

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
+// TODO make the port configurable. Take the env variable from docker-compose
+// TODO make host configurable
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:6969/api', // TODO make the port configurable. Take the env variable from docker-compose // TODO make host configurable
+  baseURL: 'http://localhost:6969/api',
 };
