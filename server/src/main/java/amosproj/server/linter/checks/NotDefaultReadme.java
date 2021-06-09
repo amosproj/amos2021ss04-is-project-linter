7e331055f7d109fc3306b25631766c9d94ecb7c8
Lukas <boehm-lukas@outlook.de>
2021-06-09 20:04:47 +0200

    :100: create all the classes

---
+++ server/src/main/java/amosproj/server/linter/checks/NotDefaultReadme.java
@@ -0,0 +1,10 @@
+package amosproj.server.linter.checks;
+
+public class NotDefaultReadme extends Check {
+
+    @Override
+    protected boolean evaluate() {
+        System.out.println("ich bin in der run methode");
+        return false;
+    }
+}
