3ad24b12064d414de6fd2bbb78b1819cf9503493
Lukas <boehm-lukas@outlook.de>
2021-06-20 00:00:03 +0200

    :construction:  API service

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -5,7 +5,7 @@ import {
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -17,8 +17,46 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  allProjects(): Observable<any> {
+  // All Project
+  getAllProjects(extended: boolean): Observable<any> {
     return this.http
       .get(`${this.apiUrl}/projects`)
+      .pipe(catchError(this.error));
+  }
+
+  // One Project
+  getProject(id: number): Observable<any> {
+    return this.http
+      .get(`${this.http}/project/${id}`)
+      .pipe(catchError(this.error));
+  }
+
+  // Start Crawler
+  startCrawler(): Observable<any> {
+    return this.http
+      .post(`${this.apiUrl}/crawler`, null)
+      .pipe(catchError(this.error));
+  }
+
+  // Crawler Status
+  crawlerStatus(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/crawler`).pipe(catchError(this.error));
+  }
+
+  // Config
+  getConfig(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/config`).pipe(catchError(this.error));
+  }
+
+  // Handle Errors
+  error(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,6 +13,7 @@ import { RepositoryComponent } from './repository/repository.component';
 import { SpinnerComponentComponent } from './spinner-component/spinner-component.component';
 import { Chart } from 'chart.js';
 import { Project, Config, CheckResults, LintingResult } from './schemas';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -85,7 +86,8 @@ export class AppComponent implements OnInit {
     public dialog: MatDialog,
     fb: FormBuilder,
     private _cfr: ComponentFactoryResolver,
-    private http: HttpClient
+    private http: HttpClient,
+    private apiService: ApiService
   ) {
     this.options = fb.group({
       hideRequired: this.hideRequiredControl,
@@ -96,14 +98,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.GetConfig();
     this.GetProjects();
-  }
-
-  manuallyStartCrawler() {
-    this.http
-      .post(`${environment.baseURL}/crawler`, null)
-      .subscribe((result: any) => {
-        console.log(result);
-      });
   }
 
   ngAfterViewInit() {}
