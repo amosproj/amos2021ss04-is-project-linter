f04ec314760c932461ca71f7aae95c36f884e63d
Lukas <boehm-lukas@outlook.de>
2021-06-20 22:59:32 +0200

    :recycle: extract header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RepositoryComponent } from './repository/repository.component';
 import { RepositoryDetailsComponent } from './repository-details/repository-details.component';
 import { SpinnerComponentComponent } from './spinner-component/spinner-component.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FooterComponent } from './footer/footer.component';
     RepositoryDetailsComponent,
     SpinnerComponentComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   entryComponents: [RepositoryDetailsComponent],
   imports: [
