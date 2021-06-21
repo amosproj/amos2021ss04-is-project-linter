84a1a98561d20003ca108b56d4f04e155cb5dea2
Christoph Wohlwend <c.wohlwend@gmx.de>
2021-06-21 04:02:03 +0200

    :sparkles: integrated the values of the api call into the chart, code needs some refactoring

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { SpinnerComponentComponent } from './spinner-component/spinner-component
 import { Chart } from 'chart.js';
 import { Project, Config, CheckResults, LintingResult } from './schemas';
 import { ApiService } from './api.service';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
@@ -68,15 +69,28 @@ export class AppComponent implements OnInit {
   chartOptionsCheckPerCategoriePercantage;
 
 
-  chartcolors = {
-    red: 'rgb(255, 99, 132)',
-    orange: 'rgb(255, 159, 64)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(75, 192, 192)',
-    blue: 'rgb(54, 162, 235)',
-    purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(231,233,237)',
-  };
+  chartColors = [
+    //red: 
+    'rgb(255, 99, 132)',
+    //orange: 
+    'rgb(255, 159, 64)',
+    //yellow: 
+    'rgb(255, 205, 86)',
+    //green: 
+    'rgb(75, 192, 192)',
+    //blue: 
+    'rgb(54, 162, 235)',
+    //purple: 
+    'rgb(153, 102, 255)',
+    //grey: 
+    'rgb(231,233,237)',
+  ];
+
+  timestamps : string[] = new Array();
+  tags : String[] = new Array();
+  values : number[][] = new Array();
+
+  seriesValues : number[][];
 
   suchBegriff: string;
   @ViewChild('parent', { read: ViewContainerRef }) container: ViewContainerRef;
@@ -364,6 +378,8 @@ export class AppComponent implements OnInit {
   }
 
   setChartData() {
+    console.log('timestamps',this.timestamps);
+
     this.dataImportantChecks = {
       labels: [
         '01.01.2020',
@@ -378,25 +394,25 @@ export class AppComponent implements OnInit {
         {
           label: 'Top 5',
           data: [10, 11, 11, 11, 12, 14, 14],
-          backgroundColor: this.chartcolors.red,
-          borderColor: this.chartcolors.red,
-          pointRadius: '0',
-          fill: '1',
+          backgroundColor: this.chartColors[0],
+          borderColor: this.chartColors[0],
+          pointRadius: 0,
+          fill: 1,
         },
         {
           label: 'Top 10',
           data: [7, 7, 7, 7, 8, 8, 9],
-          backgroundColor: this.chartcolors.blue,
-          borderColor: this.chartcolors.blue,
-          pointRadius: '0',
-          fill: '2',
+          backgroundColor: this.chartColors[1],
+          borderColor: this.chartColors[1],
+          pointRadius: 0,
+          fill: 2,
         },
         {
           label: 'Top 15',
           data: [5, 6, 6, 7, 7, 7, 8],
-          backgroundColor: this.chartcolors.green,
-          borderColor: this.chartcolors.green,
-          pointRadius: '0',
+          backgroundColor: this.chartColors[2],
+          borderColor: this.chartColors[2],
+          pointRadius: 0,
           fill: true
         }
       ],
@@ -472,16 +488,6 @@ export class AppComponent implements OnInit {
       }
     };
 
-    this.dataCheckPerCategorie = {
-      labels: ['0d', '10d', '20d', '30d', '40d', '50d', '60d'],
-      datasets: [
-        {
-          label: 'Car Color',
-          data: [0, 10, 5, 50, 20, 70, 45],
-        },
-      ],
-    };
-
     this.dataCheckPerCategoriePercentage = {
       labels: ['0d', '10d', '20d', '30d', '40d', '50d', '60d'],
       datasets: [
@@ -553,8 +559,53 @@ export class AppComponent implements OnInit {
   }
 
   setOnStatistikTab() {
-    this.setChartData();
-    this.renderStatistikCharts();
+
+    this.getChartData();
+  }
+
+  async getChartData(){
+    await this.http.get(`${environment.baseURL}/projects/allCategories`)
+    .toPromise()
+    .then((results: any) => {
+      //console.log('new api returns',results);
+      for (let x in results){
+        //console.log('timestamp',x);
+        if(this.timestamps.includes(dayjs(x).format('DD.MM.YYYY'))){
+          continue;
+        }
+        this.timestamps.push(dayjs(x).format('DD.MM.YYYY'));
+        var value : number[] = new Array();
+        for(let y in results[x]){
+          //console.log('tag',y);
+          if(!this.tags.includes(y)){
+            this.tags.push(y);
+          }
+          //console.log('value',results[x][y]);
+          value.push(results[x][y]);
+        }
+        this.values.push(value);
+      }
+      //console.log(this.timestamps);
+      //console.log(this.tags);
+      //console.log(this.values);
+
+      this.seriesValues = new Array(this.tags.length);
+      for (var i = 0; i < this.tags.length; i++){
+        this.seriesValues[i] = new Array(this.timestamps.length);
+      }
+
+      for (var i = 0; i < this.timestamps.length; i++){
+        for (var j = 0; j < this.tags.length; j++){
+          this.seriesValues[j][i] = this.values[i][j];
+        }
+      }
+
+      console.log('timestamps in http',this.timestamps);
+      
+
+      this.setChartData();
+      this.renderStatistikCharts();
+    });
   }
 
   renderStatistikCharts() {
@@ -596,12 +647,45 @@ export class AppComponent implements OnInit {
       this.canvasCheckPerCategorie.getContext('2d'),
       {
         type: 'line',
-        data: this.dataCheckPerCategorie,
         options: this.chartOptionsCheckPerCategorie,
       }
     );
 
+    //console.log('timestamp in renderChart',this.timestamps);
+    this.chartCheckPerCategorie.data.labels = this.timestamps;
+
+    var datasets = [];
+    for(var i = 0; i < this.tags.length; i++){
+      if(i != this.tags.length - 1){
+        datasets.push({label: this.tags[i],
+          data: this.seriesValues[i], 
+          backgroundColor: this.chartColors[i%this.chartColors.length],
+          borderColor: this.chartColors[i%this.chartColors.length],
+          pointRadius: 0,
+          fill: i + 1,
+         });
+      } else {
+        datasets.push({label: this.tags[i],
+          data: this.seriesValues[i], 
+          backgroundColor: this.chartColors[i%this.chartColors.length],
+          borderColor: this.chartColors[i%this.chartColors.length],
+          pointRadius: 0,
+          fill: true,
+         });
+      }
+    }
+    //test.push({label: this.tags[0], data: this.seriesValues[0]})
+    //var test = [{label: this.tags[0], data: this.seriesValues[0]}];
+
+    //this.chartCheckPerCategorie.data.datasets = [{label : 'Series1', data: [1,3]}];
+
+
+    this.chartCheckPerCategorie.data.datasets = datasets;
+
     this.chartCheckPerCategorie.update();
+
+    //console.log(this.chartCheckPerCategorie.data.labels);
+    //console.log('Dataset',this.chartCheckPerCategorie.data.datasets);
 
     this.canvasCheckPerCategoriePercantage = <HTMLCanvasElement>(
       document.getElementById('checksPerCategoriePercentage')
@@ -611,15 +695,31 @@ export class AppComponent implements OnInit {
       this.canvasCheckPerCategoriePercantage.getContext('2d'),
       {
         type: 'line',
-        data: this.dataCheckPerCategoriePercentage,
         options: this.chartOptionsCheckPerCategoriePercantage,
       }
     );
 
+    for(var i = 0; i < this.tags.length; i++){
+      datasets[i].data = datasets[i].data.map(x => x * 100 / this.all_projects.length);
+      datasets[i].fill = false;
+    }
+
+    this.chartCheckPerCategoriePercentage.data.labels = this.timestamps;
+    this.chartCheckPerCategoriePercentage.data.datasets = datasets;
+
     this.chartCheckPerCategoriePercentage.update();
+    console.log('Dataset Percentage',this.chartCheckPerCategoriePercentage.data.datasets)
   }
 
   toggleSelection(chip: MatChip) {
     chip.toggleSelected();
   }
+
+  addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
 }
