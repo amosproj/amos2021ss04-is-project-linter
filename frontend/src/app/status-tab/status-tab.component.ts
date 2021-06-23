64d31484599edb0e95bff3fd14bc5792c479e53d
Lukas <boehm-lukas@outlook.de>
2021-06-23 18:48:47 +0200

    :sparkles: new status component

---
+++ frontend/src/app/status-tab/status-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-tab',
+  templateUrl: './status-tab.component.html',
+  styleUrls: ['./status-tab.component.css']
+})
+export class StatusTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
