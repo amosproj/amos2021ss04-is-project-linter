2f5851b011002fcb41e61594337651cde5c35373
Lukas <boehm-lukas@outlook.de>
2021-06-29 02:12:21 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-// TODO make the port configurable. Take the env variable from docker-compose
-// TODO make host configurable
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:6969/api',
+  baseURL: window.location.origin + '/api',
 };
