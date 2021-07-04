74e7bbe728f056d8a5c7fbcd3ebcaa8cdd93b554
Lukas <boehm-lukas@outlook.de>
2021-07-05 00:23:21 +0200

    :construction: keep going

--- data/generate.py
+++ data/generate.py
@@ -1,11 +1,35 @@
 from datetime import datetime, timedelta, timezone
+import random
+import json
 
 start_time = datetime(2021, 6, 6, 15, 0, 0)
 dates = [start_time + timedelta(n) for n in range(7)]
 
+checks = [
+    'CheckContributingExistence',
+    'EitherOwnersOrMaintainersExist',
+    'GuestRoleDisabled',
+    'HasSquashingDisabled',
+    'HasAvatar',
+    'CheckReadmeHasLinks',
+    'CheckReadmeHasPicture',
+    'HasForkingEnabled',
+    'GitlabWikiDisabled',
+    'HasServiceDeskDisabled',
+    'HasBadges',
+    'HasDescription',
+    'IsPublic',
+    'CheckReadmeExistence',
+    'DeveloperRoleDisabled',
+    'CheckNoContributingChain',
+    'HasIssuesEnabled',
+    'NotDefaultReadme',
+    'HasMergeRequestEnabled',
+]
+
 projects = [
     {
-        "id": 68955,
+        "id": 1,
         "description": "Test repo where we want everything to be true when linted.",
         "fork_count": 0,
         "gitlab_project_id": 19386,
@@ -13,6 +37,16 @@ projects = [
         "name": "AllChecksTrue",
         "name_space": "uv59uxut",
         "url": "https://gitlab.cs.fau.de/uv59uxut/allcheckstrue"
+    },
+    {
+        "id": 2,
+        "description": "Mixture Density Networks for PyTorch",
+        "fork_count": 0,
+        "gitlab_project_id": 19277,
+        "last_commit": "2021-06-04 15:04:03.972000",
+        "name": "pytorch-mdn",
+        "name_space": "oc81adow",
+        "url": "https://gitlab.cs.fau.de/oc81adow/pytorch-mdn"
     }
 ]
 linting_result_id = 0
@@ -30,5 +64,23 @@ for project in projects:
             "project_id": project['id']
         })
 
+# generate check results
+for lr in linting_results:
+    for check in checks:
+        check_result_id += 1
+        check_results.append({
+            "id": check_result_id,
+            "check_name": check,
+            "lint_id": lr['id'],
+            "result": bool(random.getrandbits(1)),
+        })
 
-for lr in linting_results: print(lr)
+# export shit
+with open('projects.json', 'w', encoding='UTF8') as f:
+    json.dump(projects, f)
+
+with open('linting_results.json', 'w', encoding='UTF8') as f:
+    json.dump(linting_results, f)
+
+with open('check_results.json', 'w', encoding='UTF8') as f:
+    json.dump(check_results, f)
